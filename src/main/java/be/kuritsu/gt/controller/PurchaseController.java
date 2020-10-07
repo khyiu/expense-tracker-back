a3f357b8ec7835a07168628bb7b4c794bd41d7a3
khyiu <superk888@gmail.com>
2020-10-07 23:42:27 +0200

    #53 Register purchase -> WIP: test persistence in DynamoDB

--- src/main/java/be/kuritsu/gt/controller/PurchaseController.java
+++ src/main/java/be/kuritsu/gt/controller/PurchaseController.java
@@ -2,14 +2,17 @@ package be.kuritsu.gt.controller;
 
 import be.kuritsu.gt.api.PurchasesApi;
 import be.kuritsu.gt.model.Purchase;
+import be.kuritsu.gt.model.PurchaseEntity;
 import be.kuritsu.gt.model.PurchaseRequest;
 import be.kuritsu.gt.repository.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class PurchaseController implements PurchasesApi {
@@ -20,6 +23,12 @@ public class PurchaseController implements PurchasesApi {
     @Secured("ROLE_USERS")
     @Override
     public ResponseEntity<Purchase> registerPurchase(@Valid PurchaseRequest purchaseRequest) {
-        return null;
+        PurchaseEntity purchaseEntity = PurchaseEntity.builder()
+                .date(purchaseRequest.getDate().format(DateTimeFormatter.ISO_DATE))
+                .brand(purchaseRequest.getBrand())
+                .build();
+
+        PurchaseEntity savedPurchaseEntity = purchaseRepository.save(purchaseEntity);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
     }
 }
--- src/test/java/integration/PurchaseIT.java
+++ src/test/java/integration/PurchaseIT.java
@@ -5,6 +5,7 @@ import be.kuritsu.gt.model.Packaging;
 import be.kuritsu.gt.model.PurchaseLocation;
 import be.kuritsu.gt.model.PurchaseRequest;
 import be.kuritsu.gt.model.UnitMeasurement;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,7 +13,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -24,6 +27,7 @@ import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {
@@ -46,16 +50,15 @@ public class PurchaseIT {
                 .build();
     }
 
-    @Test
-    public void test_register_purchase_unauthenticated_user() throws Exception {
-        PurchaseRequest purchaseRequest = new PurchaseRequest()
+    private PurchaseRequest getDefaultPurchaseRequest() {
+        return new PurchaseRequest()
                 .date(LocalDate.of(2020, 9, 14))
                 .location(
                         new PurchaseLocation()
                                 .description("Provigo")
                                 .locationTag("Montreal")
                 )
-                .brand("Ben & Jerrys")
+                .brand("Ben & Jerry's")
                 .descriptionTags(Collections.singletonList("Cookie dough"))
                 .unitPrice(BigDecimal.valueOf(10.0))
                 .packaging(
@@ -67,13 +70,37 @@ public class PurchaseIT {
                                                 .type(UnitMeasurement.TypeEnum.ML)
                                 )
                 );
+    }
 
+    @Test
+    public void test_register_purchase_unauthenticated_user() {
         Exception thrownException = Assert.assertThrows(Exception.class, () -> {
             mockMvc.perform(post("/purchases")
                     .contentType("application/json")
-                    .content(objectMapper.writeValueAsString(purchaseRequest)));
+                    .content(objectMapper.writeValueAsString(getDefaultPurchaseRequest())));
         });
 
         assertThat(thrownException).hasCauseInstanceOf(AuthenticationCredentialsNotFoundException.class);
+    }
+
+    @Test
+    @WithMockUser(roles = "GUEST")
+    public void test_register_purchase_unauthorized_user() {
+        Exception thrownException = Assert.assertThrows(Exception.class, () -> {
+            mockMvc.perform(post("/purchases")
+                    .contentType("application/json")
+                    .content(objectMapper.writeValueAsString(getDefaultPurchaseRequest())));
+        });
+
+        assertThat(thrownException).hasCauseInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    @WithMockUser(roles = "USERS")
+    public void test_register_purchase() throws Exception {
+        mockMvc.perform(post("/purchases")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(getDefaultPurchaseRequest())))
+                .andExpect(status().isCreated());
     }
 }
